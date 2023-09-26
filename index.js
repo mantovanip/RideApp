6c891451b08bbd0cf336472893c5e748e32e6fbe
mantovanip <mantovaniiip@gmail.com>
2023-09-25 23:09:42 -0300

    att

---
+++ index.js
@@ -0,0 +1,55 @@
+const rideListElement = document.querySelector("#rideList")
+const allRides = getAllRides()
+
+allRides.forEach(async ([id, value]) => {
+    const ride = JSON.parse(value)
+    ride.id = id
+
+    const itemElemet = document.createElement("li")
+    itemElemet.id = ride.id
+    itemElemet.className = "d-flex p-1 align-items-center justify-content=between shadow-sm gap-3"
+    rideListElement.appendChild(itemElemet)
+
+    itemElemet.addEventListener("click", () => {
+        window.location.href = `./detail.html?id=${ride.id}`
+    })
+
+    const firstPosition = ride.data[0]
+    const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude)
+
+    const mapElement = document.createElement("div")
+    mapElement.style = "width:100px;height:100px"
+    mapElement.classList.add("bg-secondary")
+    mapElement.classList.add("rounded-4")
+    
+    const dataElement = document.createElement("div")
+    dataElement.className = "flex-fill d-flex flex-column"
+
+    const cityDiv = document.createElement("div")
+    cityDiv.innerText = `${firstLocationData.city}-${firstLocationData.countryCode}`
+    cityDiv.className = "text-primary mb-2"
+
+    const maxSpeedDiv = document.createElement("div")
+    maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`
+    maxSpeedDiv.className = "h5"
+
+    const distanceDiv = document.createElement("div")
+    distanceDiv.innerText = `Distance: ${getDistance(ride.data)}`
+
+    const durationDiv = document.createElement("div")
+    durationDiv.innerText = `Duration: ${getDuration(ride)}`
+
+    const dateDiv = document.createElement("div")
+    dateDiv.innerText = getStartDate(ride)
+    dateDiv.className = "text-secondary mt-2 "
+
+    dataElement.appendChild(cityDiv)
+    dataElement.appendChild(maxSpeedDiv)
+    dataElement.appendChild(distanceDiv)
+    dataElement.appendChild(durationDiv)
+    dataElement.appendChild(dateDiv)
+    
+    itemElemet.appendChild(mapElement)
+    itemElemet.appendChild(dataElement)
+
+})
